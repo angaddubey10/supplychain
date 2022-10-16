5163580cbddb87b0b876131df6cf3c5237dc7462
Angad Dubey <angaddubey@microsoft.com>
2022-10-16 17:06:26 +0530

    Added Login

---
+++ src/main/java/com/example/supplychain/Login.java
@@ -0,0 +1,55 @@
+package com.example.supplychain;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLOutput;
+
+public class Login {
+
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    static String getEncriptedPassword(String passwordText) throws NoSuchAlgorithmException {
+        try{
+            BigInteger number = new BigInteger(1, getSHA(passwordText));
+
+            StringBuilder hexString = new StringBuilder(number.toString(16));
+//        while (hexString.length())
+            return hexString.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static boolean customerLogin(String email, String password) throws NoSuchAlgorithmException {
+        try{
+            DatabaseConnection dbCon = new DatabaseConnection();
+            String encryptedPassword = getEncriptedPassword(password);
+            String query = String.format("SELECT * FROM customer WHERE email = '%s' AND password = '%s' ", email,encryptedPassword);
+            ResultSet rs = dbCon.getQueryTable(query);
+            if(rs.next()){
+                return  true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+//        String pass = "angad123";
+//        System.out.println(pass);
+//        System.out.println(Login.getEncriptedPassword(pass));
+
+        System.out.println(Login.customerLogin("accio@gmail.com", "an6t5gad123"));
+    }
+
+
+}
